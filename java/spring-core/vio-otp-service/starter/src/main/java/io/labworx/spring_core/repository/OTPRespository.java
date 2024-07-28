2f54177f13680603d3c27d5cb05e63ce26bfa76d
Diamond Mohanty <memohanty.diamond@gmail.com>
2024-07-28 02:38:58 -0400

    Delete java/spring-core/vio-otp-service/starter directory deleted

--- java/spring-core/vio-otp-service/starter/src/main/java/io/labworx/spring_core/repository/OTPRespository.java
+++
@@ -1,7 +0,0 @@
-package io.labworx.spring_core.repository;
-
-public interface OTPRespository {
-
-    int getOTP(int seed);
-
-}
--- java/spring-core/vio-otp-service/starter/src/main/java/io/labworx/spring_core/service/VioOTPService.java
+++
@@ -1,7 +0,0 @@
-package io.labworx.spring_core.service;
-
-public interface VioOTPService {
-
-    int getOTP(int seed);
-
-}
